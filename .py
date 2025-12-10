a17cc6199f7adcc3852f8aa66f16f89427ffbea7
Adams123455 <162189760+Adams123455@users.noreply.github.com>
2025-12-10 17:46:23 +0100

    Add initial music playlist and top artists

---
+++ .py
@@ -0,0 +1,32 @@
+#work
+#1 
+playlist = ["Shower", "Vera", "Super Trouper", "Money", "Top"]
+
+#create a tuple 2
+top_artists = ("Kidi", "ABBA", "Raye", "BTS", "Nathaniel")
+
+
+#3
+my_music = [playlist, top_artists]
+print(my_music)
+
+
+#4
+print((len(my_music)))
+
+#5
+print(playlist[2]) 
+print("Third song : " + playlist[2])
+
+#6
+playlist[0] = "Perfect"
+print(playlist)
+
+
+#7
+playlist.append("Blinding Lights")
+print(playlist)
+
+
+#8
+print(playlist)
